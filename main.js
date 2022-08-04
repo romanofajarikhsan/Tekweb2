50f9f7a43a9da11d36ab36f91c70a0d920145e2b
romanofajarikhsan <102268477+romanofajarikhsan@users.noreply.github.com>
2022-08-04 15:48:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ Vue.createApp({
         getHeaderData()
         {
           axios
-            .get(apiUrl+""https://raw.githubusercontent.com/romanofajarikhsan/Tekweb2/blob/main/content/header.json"")
+            .get(apiUrl+"https://raw.githubusercontent.com/romanofajarikhsan/Tekweb2/blob/main/content/header.json")
             .then((res) => {              
               this.header = res.data;
               this.getArticles();
